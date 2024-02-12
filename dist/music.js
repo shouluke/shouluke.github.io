d1e48cdc784e19d449239d70762fceae577db87e
shouluke <2558688118@qq.com>
2024-02-12 15:40:17 +0800

    Site updated: 2024-02-12 15:40:17

---
+++ dist/music.js
@@ -0,0 +1,58 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    autoplay: false,
+    loop: 'all',
+    volume: 0.7,
+    listFolded: true,
+    listMaxHeight: 60,
+    audio: [
+        {
+            name: '温兆伦-随缘',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=2116134837.mp3',
+            cover: 'http://p1.music.126.net/Ifb9m257XUlCfkUk9rYuGw==/109951169248998695.jpg?param=130y130',
+        },
+       {
+            name: '莫忆君-梦的地方 （纯音乐）',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=2103389092.mp3',
+            cover: 'http://p1.music.126.net/_rOHBcLTDUt3_dbc6NcqNA==/109951169098698897.jpg?param=130y130',
+        },
+        {
+            name: '痴情冢（纯音乐）',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=152935855.mp3',
+            cover: 'http://p1.music.126.net/ACx8FuJZn0rwVBkyV3lmZg==/109951167952261699.jpg?param=177y177',
+        },
+        {
+            name: 'Two Steps From Hell / Thomas Bergersen-Skyworld',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=1467670634.mp3',
+            cover: 'http://p1.music.126.net/CmZX_0iEL0ataIQbS_gReg==/109951165193816103.jpg?param=130y130',
+        },
+       {
+            name: '林键标-暖雨',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=34731046.mp3',
+            cover: 'http://p1.music.126.net/0LMLmnXaG4wVih90R8f6jw==/18288176904876652.jpg?param=130y130',
+        },
+        {
+            name: '卢冠廷 / 莫文蔚-一生所爱',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=393703.mp3',
+            cover: 'http://p2.music.126.net/qHKvdeYzTqIOxcGKnrfaPQ==/72567767449767.jpg?param=130y130',
+        },
+        {
+            name: '梁雁翎-离开那个不爱你的人',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=264151.mp3',
+            cover: 'http://p1.music.126.net/DT_fHD6Aq0vkDRYKRCsKmg==/109951166656530543.jpg?param=130y130',
+        },
+        {
+            name: 'Schiller-Metropolis（Single Edit）',
+            artist: '收录客',
+            url: 'http://music.163.com/song/media/outer/url?id=1817245482.mp3',
+            cover: 'http://p1.music.126.net/Dzok-koT0Q8c5asYc3HQdg==/109951165689102525.jpg?param=130y130',
+        },
+    ]
+});
