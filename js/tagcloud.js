07ea14944ae5b017e7b3fde53d4fe63c8efdf907
shouluke <2558688118@qq.com>
2024-06-14 11:28:38 +0800

    Site updated: 2024-06-14 11:28:37

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -15,7 +15,7 @@
 
      try {
          TagCanvas.textFont = 'Trebuchet MS, Helvetica';
-         TagCanvas.textColour = '#333';
+         TagCanvas.textColour = '#FF0000';
          TagCanvas.textHeight = 25;
          TagCanvas.outlineColour = '#E2E1D1';
          TagCanvas.maxSpeed = 0.5;
